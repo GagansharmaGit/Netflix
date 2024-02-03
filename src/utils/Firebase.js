6b436d7f6cf306ce658f1f53f88a6bcffe0baf2d
Gagan Sharma <sharmagagan192@gmail.com>
2024-02-03 10:51:07 +0530

    Update Firebase.js

--- src/utils/Firebase.js
+++ src/utils/Firebase.js
@@ -9,13 +9,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "netflix-c70fc.firebaseapp.com",
-  projectId: "netflix-c70fc",
-  storageBucket: "netflix-c70fc.appspot.com",
-  messagingSenderId: "296578317972",
-  appId: "1:296578317972:web:3f9985caf85f992eebbe81",
-  measurementId: "G-PY4WDP44F0"
+  apiKey: "your api key",
+  authDomain: "firebaseapp.com",
+  projectId: "Your project id from firebase",
+  storageBucket: "appspot.com",
+  messagingSenderId: "Msg sender id from firebase",
+  appId: "App id from firebase",
+  measurementId: "44F0"
 };
 
 // Initialize Firebase
